f1f9abcccea512e6a724a652bdbc5bd66207da35
Yanka Garcia <yankagarciasoares@gmail.com>
2026-01-23 14:26:18 -0300

    AAA - Arrange, Act, Assert - Arrange: Preparar o teste, Act: Executar o teste, Assert: Verificar o resultado

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,21 +1,21 @@
 import { test, expect } from '@playwright/test'
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
   
-  // Checkpoint 1: Verificar se a página está online
+    ///AAA - Arrange, Act, Assert - Arrange: Preparar o teste, Act: Executar o teste, Assert: Verificar o resultado
+
+  // Arrange
+  await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
 
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  // Checkpoint 2: Verificar se a página de consulta de pedidos está online
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
-  // Checkpoint 3: Preencher o campo de busca com o número do pedido
+  // Act
   await page.getByTestId('search-order-id').fill('VLO-UG7N2V')
-
   await page.getByTestId('search-order-button').click()
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-UG7N2V')
 
