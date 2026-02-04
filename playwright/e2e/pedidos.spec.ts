86fde924804744774d4848dd8c74d2ab74a1c5ba
Yanka Garcia <yankagarciasoares@gmail.com>
2026-02-04 13:46:36 -0300

    Validação de Interface Além do Tradicional com Snapshots

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -27,13 +27,34 @@ test.describe('Consulta de Pedido', ()=> {
 
   // Assert
 
-    const containerPedido = page.getByRole('paragraph')
-     .filter({hasText: /^Pedido$/})
-     .locator('..') // Sobe para o elemento pai (a div que agrupa ambos) 
-
-    await expect(containerPedido).toContainText(order, {timeout: 10_000})
-
-    await expect(page.getByText('APROVADO')).toBeVisible()
+    await expect(page.getByTestId('order-result-${order}')).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order}
+      - img
+      - text: APROVADO
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: Glacier Blue
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: aero Wheels
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: Yanka Garcia
+      - paragraph: Email
+      - paragraph: garcia@velo.dev
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: À Vista
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+      `);
 
   })
 
