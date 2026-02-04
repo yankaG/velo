658dd697ca3f11cc03f532c8f09f61e9162186f1
Yanka Garcia <yankagarciasoares@gmail.com>
2026-02-03 21:09:55 -0300

    Testanto estados de erro, usando snapshot

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -30,3 +30,24 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByText('APROVADO')).toBeVisible()
 
 })
+
+test('deve exibir mensagem quando o pedido não é encontrado', async ({page})=> {
+
+  const order = 'VLO-ABC123'
+
+  await page.goto('http://localhost:5173/')
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click({timeout: 10_000})
+
+  await expect(page.locator('#root')).toMatchAriaSnapshot(`
+    - img
+    - heading "Pedido não encontrado" [level=3]
+    - paragraph: Verifique o número do pedido e tente novamente
+    `)
+
+})
