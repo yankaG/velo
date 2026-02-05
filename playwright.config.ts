a64749debfeaee5b09a2557fb787b7b819aa84c6
Yanka Garcia <yankagarciasoares@gmail.com>
2026-02-05 11:03:53 -0300

    Trace do Playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,7 +39,7 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',  //para validar somente os testes que falharam "retain-on-failure"
 
     // Tempo máximo para ações interativas como click() fill()
     // Quando o valor é 0, herda o limite do timeout geral do teste
