736f2330d8a109534214141a76f9bca074f72111
Yanka Garcia <yankagarciasoares@gmail.com>
2026-02-10 15:48:46 -0300

    IA no processamento de Testes

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -122,6 +122,60 @@ test.describe('Consulta de Pedido', ()=> {
     `);
   
   })
+
+  test('deve consultar um pedido em analise', async ({ page }) => {
+
+    // Test Data
+    //const order = 'VLO-ONDYEX'
+    const order = {
+      number: 'VLO-GM9NR5',
+      status: 'EM_ANALISE',
+      color: 'Glacier Blue',
+      wheels: 'aero Wheels',
+      customer: {
+        name: 'Lunar Miler',
+        email: 'lunar@velo.com',
+      },
+      payment: 'À Vista'
+    }
+    // Act
+    
+    await page.getByRole('textbox', { name: 'Número do Pedido' }).fill(order.number)
+    await page.getByRole('button', { name: 'Buscar Pedido' }).click({timeout: 10_000})
+  
+    // Assert
+  
+    await expect(page.getByTestId(`order-result-${order.number}`)).toMatchAriaSnapshot(`
+      - img
+      - paragraph: Pedido
+      - paragraph: ${order.number}
+      - img
+      - text: ${order.status}
+      - img "Velô Sprint"
+      - paragraph: Modelo
+      - paragraph: Velô Sprint
+      - paragraph: Cor
+      - paragraph: ${order.color}
+      - paragraph: Interior
+      - paragraph: cream
+      - paragraph: Rodas
+      - paragraph: ${order.wheels}
+      - heading "Dados do Cliente" [level=4]
+      - paragraph: Nome
+      - paragraph: ${order.customer.name}
+      - paragraph: Email
+      - paragraph: ${order.customer.email}
+      - paragraph: Loja de Retirada
+      - paragraph
+      - paragraph: Data do Pedido
+      - paragraph: /\\d+\\/\\d+\\/\\d+/
+      - heading "Pagamento" [level=4]
+      - paragraph: ${order.payment}
+      - paragraph: /R\\$ \\d+\\.\\d+,\\d+/
+    `);
+  
+  })
+
   test('deve exibir mensagem quando o pedido não é encontrado', async ({page})=> {
 
     const order = gerarCodigoPedido()
